c2c044879d7be2a6c8d9ffc9056c5bfec3646d31
vincentlaurens <18136724+vincentlaurens@users.noreply.github.com>
2018-10-05 16:47:09 +0200

    Update router_config_creation.py
    first modif of script router configuration

--- router_config_creation.py
+++ router_config_creation.py
@@ -11,4 +11,11 @@ from constants import PREFIXES, PATH, VLAN_USES
 with open(PATH+'router_configuration.json') as data_file:
     data = json.load(data_file)
     
+  ######################## #################router_start config ################################################
+
+    router_start_config = open(PATH+"project_cfg/"+router+"_start", "w")
+    router_start_config.write("#!/bin/bash \n\n")
+    router_start_config.write("# This file has been generated automatically, see router_config_creation.py for details. \n\n")
+
+    
 
