2f8949b99784f2f1cedc9aeccfc6e7037db59c24
vincentlaurens <18136724+vincentlaurens@users.noreply.github.com>
2018-10-07 15:49:23 +0200

    Update launch_network_group3.sh
    add creation of bird repository

--- launch_network_group3.sh
+++ launch_network_group3.sh
@@ -2,27 +2,20 @@
 
 # Cleanup
 sudo ./cleanup.sh
-#mkdir -p project_cfg/MONI/log
+
 mkdir -p group3_cfg/Halles/log
-#mkdir -p project_cfg/MICH/log
 mkdir -p group3_cfg/Pythagore/log
-#mkdir -p project_cfg/STEV/log
-#mkdir -p project_cfg/SH1C/log
-#mkdir -p project_cfg/CARN/log
-#mkdir -p /var/cache/bind/ns1
-#mkdir -p /var/cache/bind/ns2
 
 
-#sudo echo > project_cfg/MONIT/log/services_status_log
-#sudo echo > project_cfg/MONIT/log/isp_status_log
+
 sudo touch group3_cfg/Halles/log/bird_log
 sudo touch group3_cfg/Halles/log/backup_link_log
 sudo touch group3_cfg/Pythagore/log/bird_log
 sudo touch group3_cfg/Pythagore/log/backup_link_log
-#sudo echo > project_cfg/STEV/log/bird_log
-#sudo echo > project_cfg/CARN/log/bird_log
-#sudo echo > project_cfg/MICH/log/bird_log
-#sudo echo > project_cfg/SH1C/log/bird_log
+
+#creation of routers directories
+mkdir -p group3_cfg/Halles/bird
+mkdir -p group3_cfg/Pythagore/bird
 
 # Configuration files creation
 sudo ./router_config_creation.py
