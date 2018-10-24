d35e188d13a23196af03a5fbce61206940645c9e
Pierrot31 <33099765+Pierrot31@users.noreply.github.com>
2018-10-24 11:12:12 +0200

    Update firewall_config_creation.py

--- firewall_config_creation.py
+++ firewall_config_creation.py
@@ -114,7 +114,7 @@ for router, configs_firewall in data.items():
 	if configs_firewall["bgp"] == "true":
 		router_firewall_config_file.write(
 		"		#allow BGP(router connected with provider)\n"
-		"		for k in 'd' 's'\n"
+		"		for k in 'd'\n"
 		"		do\n"
 		"			ip6tables -A INPUT -${k} fd00:${a}::"+configs_firewall["suffixe_provider"]+"/48 -p tcp --${k}port 179 -j ACCEPT\n"
 		"			ip6tables -A OUTPUT -${k} fd00:${a}::"+configs_firewall["suffixe_provider"]+"/48 -p tcp --${k}port 179 -j ACCEPT\n"
