1cbac37b795e61e6edfcfd03f418ea4083181bd2
Pierrot31 <33099765+Pierrot31@users.noreply.github.com>
2018-10-24 11:20:40 +0200

    Update firewall_config_creation.py

--- firewall_config_creation.py
+++ firewall_config_creation.py
@@ -114,12 +114,15 @@ for router, configs_firewall in data.items():
 	if configs_firewall["bgp"] == "true":
 		router_firewall_config_file.write(
 		"		#allow BGP(router connected with provider)\n"
-		"		for k in 'd'\n"
-		"		do\n"
-		"			ip6tables -A INPUT -${k} fd00:${a}::"+configs_firewall["suffixe_provider"]+"/48 -p tcp --${k}port 179 -j ACCEPT\n"
-		"			ip6tables -A OUTPUT -${k} fd00:${a}::"+configs_firewall["suffixe_provider"]+"/48 -p tcp --${k}port 179 -j ACCEPT\n"
-		"			ip6tables -A FORWARD -${k} fd00:${a}::"+configs_firewall["suffixe_provider"]+"/48 -p tcp --${k}port 179 -j ACCEPT\n"
-		"		done\n"
+		"ip6tables -A INPUT -p tcp -m tcp --destination-port 179 -j ACCEPT\n"
+    		"ip6tables -A OUTPUT -p tcp -m tcp --destination-port 179 -j ACCEPT\n" 
+		"ip6tables -A FORWARD -p tcp -m tcp --destination-port 179 -j ACCEPTǹ"
+		"#		for k in 'd'\n"
+		"#		do\n"
+		"#			ip6tables -A INPUT -${k} fd00:${a}::"+configs_firewall["suffixe_provider"]+"/48 -p tcp --${k}port 179 -j ACCEPT\n"
+		"#			ip6tables -A OUTPUT -${k} fd00:${a}::"+configs_firewall["suffixe_provider"]+"/48 -p tcp --${k}port 179 -j ACCEPT\n"
+		"#			ip6tables -A FORWARD -${k} fd00:${a}::"+configs_firewall["suffixe_provider"]+"/48 -p tcp --${k}port 179 -j ACCEPT\n"
+		"#		done\n"
 		)
 	router_firewall_config_file.write(
 		"		ip6tables -A OUTPUT -p udp -d fd00:${a}:3:"+configs_firewall["suffixe_DNS"]+"/64 --dport 53 -m state --state NEW,ESTABLISHED -j ACCEPT\n"
