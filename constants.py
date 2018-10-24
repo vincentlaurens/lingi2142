a2daac3eb4291924c19ea2a7149271f217145110
vlaurens <vlaurens@etud.insa-toulouse.fr>
2018-10-24 15:03:56 +0200

    Merge branch 'master' of /home/vlaurens/Documents/INSA/4_IR_RT_2019/UCLouvain/lingi2142-network-management/project_group3_der with conflicts.

--- constants.py
+++ constants.py
@@ -1,4 +1,5 @@
 #Python 3 : Define the constants
 
-PATH = "/home/vagrant/test/lingi2142/"
+#PATH = "/home/vagrant/test/lingi2142/"
+PATH = "/home/vlaurens/Documents/INSA/4_IR_RT_2019/UCLouvain/lingi2142-network-management/project_group3_der/"
 PREFIXES_ADDRESS = ["fd00:200:3:", "fd00:300:3:"]
