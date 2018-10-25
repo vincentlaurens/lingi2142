1ba2776233b5f583c0dfb0f7d2bc977431e80349
jr <rahmjan@gmail.com>
2018-10-25 19:57:49 +0200

    Monitoring

--- launch_network_group3.sh
+++ launch_network_group3.sh
@@ -52,7 +52,7 @@ sudo touch group3_cfg/Mich/log/bird_log
 # Configuration files creation
 sudo python3 router_config_creation.py
 sudo python3 firewall_config_creation.py
-#sudo ./monitoring/monitoring_config_creation.sh
+sudo ./monitoring/monitoring_config_creation.sh
 #sudo ./service_config_creation.py
 #sudo ./host_config_creation.py
 #sudo ./end_user_management/deploy_end_user_management.sh
