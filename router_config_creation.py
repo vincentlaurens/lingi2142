27c192b10467ac0c0d8c71e510cf69f8e9575c93
vlaurens <vlaurens@etud.insa-toulouse.fr>
2018-10-25 18:29:05 +0200

    test lan

--- router_config_creation.py
+++ router_config_creation.py
@@ -46,11 +46,9 @@ for router, configs in data.items():
 	router_start_file.write("\n")
 	if "lans" in configs:
 		for lan, lan_configs in configs["lans"].items():
-			pprint(lan+"\n")
-			pprint(lan_configs["id_lan"]+"\n")
-			router_start_file.write("ip link set dev "+router+"-"+lan+"-"+lan_configs["id_lan"]+" up \n")
+			router_start_file.write("ip link set dev "+router+"-"+lan+" up \n")
 			for prefix_address in PREFIXES_ADDRESS:
-				router_start_file.write("ip address add dev "+router+"-"+lan+"-"+lan_configs["id_lan"]+" "+prefix_address+lan_configs["addr"]+"::"+configs["router_id"]+"/64 \n")
+				router_start_file.write("ip address add dev "+router+"-"+lan+" "+prefix_address+lan_configs+"::"+configs["router_id"]+"/64 \n")
 
 	router_start_file.write("bird6 -s /tmp/"+router+"_bird.ctl -P /tmp/"+router+"_bird.pid \n")
 	router_start_file.close()
