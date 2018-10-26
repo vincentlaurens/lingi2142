331ea511973f809c4c0678265c7675c89189cfc3
jr <rahmjan@gmail.com>
2018-10-26 17:40:21 +0200

    Monitoring

--- router_config_creation.py
+++ router_config_creation.py
@@ -48,8 +48,9 @@ for router, configs in data.items():
 		for lan, lan_configs in configs["lans"].items():
 			router_start_file.write("ip link set dev "+router+"-"+lan+" up \n")
 			for prefix_address in PREFIXES_ADDRESS:
-				router_start_file.write("ip address add dev "+router+"-"+lan+" "+prefix_address+lan_configs+"::/64 \n")
+				router_start_file.write("ip address add dev "+router+"-"+lan+" "+prefix_address+lan_configs+"::"+configs["router_id"]+"/64 \n")
 
+	router_start_file.write("\n")
 	router_start_file.write("bird6 -s /tmp/"+router+"_bird.ctl -P /tmp/"+router+"_bird.pid \n")
 	router_start_file.close()
 	os.chmod(PATH+"group3_cfg/"+router+"_start.sh", 0o766)
