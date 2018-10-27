c3ea3bf137af3678ce5f8cdd0d2f156d4fe1856a
jr <rahmjan@gmail.com>
2018-10-27 19:42:11 +0200

    Monitoring

---
+++ monitoring/Mon/download_bird_logs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+mkdir -p /etc/log/bird_logs
+
+ROUTERS_TO_CHECK=("Hall" "Pyth" "Stev" "Carn" "Mich" "SH1C")
+
+# Wait for start of the network
+sleep 15
+
+while true
+do
+    for ROUTER in "${ROUTERS_TO_CHECK[@]}";
+    do
+        cp ./group3_cfg/${ROUTER}/log/bird_log /etc/log/bird_logs/${ROUTER}_bird_log
+    done
+
+    sleep 10
+done
+
+exit
