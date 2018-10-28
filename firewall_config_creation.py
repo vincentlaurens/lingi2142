f89a78d0b1ba861ecc2955f5287967dd04c554cb
Pierrot31 <33099765+Pierrot31@users.noreply.github.com>
2018-10-28 21:38:04 +0100

    correct Mon rules Mistakes

--- firewall_config_creation.py
+++ firewall_config_creation.py
@@ -157,7 +157,8 @@ for router, configs_firewall in data.items():
 					pprint(configs_firewall["router_id"])
 					router_firewall_config_file.write(
 					"#Allow SNMP for Monitoring LAN and mailbox protocols and SSH for check log for instance\n"
-					"ip6tables -A INPUT -p tcp -d fd00:${a}:3:"+lan_rules[0]+"::"+configs_firewall["router_id"]+"/64 -m multiport --dports 161,162 -j ACCEPT\n"
+					"ip6tables -A INPUT -p tcp -d fd00:${a}:3:"+lan_rules[0]+"::"+configs_firewall["router_id"]+"/64 -m tcp --dport 161 -j ACCEPT\n"
+					"ip6tables -A INPUT -p udp -d fd00:${a}:3:"+lan_rules[0]+"::"+configs_firewall["router_id"]+"/64 -m udp --dport 162 -j ACCEPT\n"
 					"ip6tables -A INPUT -p tcp -d fd00:${a}:3:"+lan_rules[0]+"::"+configs_firewall["router_id"]+"/64 --dport 22 -j ACCEPT\n"	
 					"ip6tables -A INPUT -p tcp -d fd00:${a}:3:"+lan_rules[0]+"::"+configs_firewall["router_id"]+"/64 -m multiport --dports 25,110,143 -j ACCEPT\n\n"	
 				)
