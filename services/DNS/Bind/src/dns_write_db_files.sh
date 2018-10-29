cf3178987aa8db36a72f1bfecca0aa6af2abb306
tanguy <ntlecorre@hotmail.fr>
2018-10-29 12:28:57 +0100

    change :53

--- services/DNS/Bind/src/dns_write_db_files.sh
+++ services/DNS/Bind/src/dns_write_db_files.sh
@@ -20,8 +20,8 @@ db_group3.write(
 
 db_group3.write("@             IN        NS        ns1.group3.ingi. \n")
 db_group3.write("@             IN        TXT       \"zone group3\" \n")
-db_group3.write("@             IN        AAAA      fd00:200:3:1000::1 \n")
-db_group3.write("@             IN        AAAA      fd00:300:3:1000::1 \n")
+db_group3.write("@             IN        AAAA      fd00:200:3:1000::53 \n")
+db_group3.write("@             IN        AAAA      fd00:300:3:1000::53 \n")
 
 db_group3.close()
 
@@ -37,7 +37,7 @@ db_group3_reverse.write(
 "	1h		;\n"
 	)
 db_group3_reverse.write("@	IN	NS	ns1.group3.ingi")
-db_group3_reverse.write("1.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.1.3.0.0.0.0.0.2.0.0.0.d.f.ip6.arpa.    IN    PTR    ns1.group3.ingi.")
+db_group3_reverse.write("3.5.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.1.3.0.0.0.0.0.2.0.0.0.d.f.ip6.arpa.    IN    PTR    ns1.group3.ingi.")
 
 
 db_group3.close()
