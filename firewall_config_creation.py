8c555897d2812a7219c685ee6a8dcd03765d1ba1
Pierrot31 <33099765+Pierrot31@users.noreply.github.com>
2018-10-29 13:02:34 +0100

    correct mistake

--- firewall_config_creation.py
+++ firewall_config_creation.py
@@ -175,9 +175,10 @@ for router, configs_firewall in data.items():
 		"		#allow BGP(router connected with provider)\n"
 		"		ip6tables -A INPUT -p tcp -m tcp --dport 179 -j ACCEPT\n"
 		"		ip6tables -A OUTPUT -p tcp -m tcp --dport 179 -j ACCEPT\n" 
-		"		ip6tables -A FORWARD -p tcp -m tcp --dport 179 -j ACCEPT\n"
+		"		ip6tables -A FORWARD -p tcp -m tcp --dport 179 -j ACCEPT\n\n"
 		)
 	router_firewall_config_file.write(
+		"		#Allow DNS server\n"
 		"		ip6tables -A OUTPUT -p udp -d fd00:${a}:3:"+configs_firewall["suffixe_DNS"]+"/64 --dport 53 -m state --state NEW,ESTABLISHED -j ACCEPT\n"
 		"		ip6tables -A INPUT  -p udp -s fd00:${a}:3:"+configs_firewall["suffixe_DNS"]+"/64 --sport 53 -m state --state ESTABLISHED     -j ACCEPT\n"
 		"		ip6tables -A OUTPUT -p tcp -d fd00:${a}:3:"+configs_firewall["suffixe_DNS"]+"/64 --dport 53 -m state --state NEW,ESTABLISHED -j ACCEPT\n"
