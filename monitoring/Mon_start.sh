b4287064bb56ed9d722b92e0508c73b69fa54aa4
plaurens <plaurens@etud.insa-toulouse.fr>
2018-10-30 15:45:44 +0100

    Mon

--- monitoring/Mon_start.sh
+++ monitoring/Mon_start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Interfaces
-ip link set dev Mon-eth0 up
+#ip link set dev Mon-eth0 up
 ip address add dev Mon-eth0 fd00:200:3:ffff::1/64
 ip address add dev Mon-eth0 fd00:300:3:ffff::1/64
 
 # Gateway
-ip -6 route add ::/0 via fd00:200:3:ffff::2/64
+ip -6 route add ::/0 via fd00:200:3:ffff::2/64 #address of pyth
 
 # Scripts
 /etc/check_isp_status.sh &
