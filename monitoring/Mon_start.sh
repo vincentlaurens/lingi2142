8cc8b84f286d3a275ef751d00898bbe8abed2f05
plaurens <plaurens@etud.insa-toulouse.fr>
2018-10-30 15:25:27 +0100

    Mon

--- monitoring/Mon_start.sh
+++ monitoring/Mon_start.sh
@@ -6,7 +6,7 @@ ip address add dev Mon-eth0 fd00:200:3:ffff::1/64
 ip address add dev Mon-eth0 fd00:300:3:ffff::1/64
 
 # Gateway
-ip -6 route add ::/0 via fd00:200:3:ffff::2
+ip -6 route add ::/0 via fd00:200:3:ffff::2/64
 
 # Scripts
 /etc/check_isp_status.sh &
