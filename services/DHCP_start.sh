fb60ad74a82443a41866df011f9c8a388deb35dd
plaurens <plaurens@etud.insa-toulouse.fr>
2018-10-31 00:21:43 +0100

    services-dhcp

--- services/DHCP_start.sh
+++ services/DHCP_start.sh
@@ -5,4 +5,4 @@
 ip address add dev Dhcp-eth0 fd00:200:3:1000::547/64
 ip address add dev Dhcp-eth0 fd00:300:3:1000::547/64
 # Gateway
-ip -6 route add ::/0 via fd00:200:3:1000::1/64 #address of Hall
+ip -6 route add ::/0 via fd00:200:3:1000::1 #address of Hall
