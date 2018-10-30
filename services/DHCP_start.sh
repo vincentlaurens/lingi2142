df33810eee6a7c6762b5f7fcd52ee93c6d90efd2
plaurens <plaurens@etud.insa-toulouse.fr>
2018-10-31 00:31:07 +0100

    dhcp correct

--- services/DHCP_start.sh
+++ services/DHCP_start.sh
@@ -2,7 +2,7 @@
 
 # Interfaces
 #DHCP server
-ip address add dev Dhcp-eth0 fd00:200:3:1000::547/64
-ip address add dev Dhcp-eth0 fd00:300:3:1000::547/64
+ip address add dev DHCP-eth0 fd00:200:3:1000::547/64
+ip address add dev DHCP-eth0 fd00:300:3:1000::547/64
 # Gateway
 ip -6 route add ::/0 via fd00:200:3:1000::1 #address of Hall
