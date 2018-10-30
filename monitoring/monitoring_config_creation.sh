04ddeaf2a7ba061e529cf03af0367f07ca24e548
plaurens <plaurens@etud.insa-toulouse.fr>
2018-10-30 21:23:17 +0100

    boot file delete

--- monitoring/monitoring_config_creation.sh
+++ monitoring/monitoring_config_creation.sh
@@ -6,5 +6,6 @@
 sudo mkdir -p ./group3_cfg/
 
 # Copy configuration
+#sudo cp ./monitoring/Mon_boot.sh ./group3_cfg/Mon_boot.sh
 sudo cp ./monitoring/Mon_start.sh ./group3_cfg/Mon_start.sh
 sudo cp -R ./monitoring/Mon ./group3_cfg/
