8f6871e90da7600684212e82d5e05c32161aef2c
plaurens <plaurens@etud.insa-toulouse.fr>
2018-10-30 22:14:50 +0100

    delete route for 300

--- monitoring/Mon_start.sh
+++ monitoring/Mon_start.sh
@@ -7,7 +7,7 @@ ip address add dev Mon-eth0 fd00:300:3:ffff::1/64
 
 # Gateway
 ip -6 route add ::/0 via fd00:200:3:ffff::2 #address of pyth
-ip -6 route add ::/0 via fd00:300:3:ffff::2
+
 
 # Scripts
 /etc/check_isp_status.sh &
