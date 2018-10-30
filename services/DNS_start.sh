41b62c10060414ec75a8464bbac1beb4b15f8cea
plaurens <plaurens@etud.insa-toulouse.fr>
2018-10-30 23:08:33 +0100

    services

--- services/DNS_start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Interfaces
-#DNS server
-ip address add dev Dns-eth0 fd00:200:3:1000::53/64
-ip address add dev Dns-eth0 fd00:300:3:1000::53/64
-# Gateway
-ip -6 route add ::/0 via fd00:300:3:1000::1/64 #address of Hall
-
