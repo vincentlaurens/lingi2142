3b90d0130ee9798211dadf6617a5b49da3c2c706
plaurens <plaurens@etud.insa-toulouse.fr>
2018-10-30 23:16:19 +0100

    services

--- launch_network_group3.sh
+++ launch_network_group3.sh
@@ -54,7 +54,6 @@ sudo python3 router_config_creation.py
 sudo python3 firewall_config_creation.py
 sudo ./monitoring/monitoring_config_creation.sh
 sudo ./services/serv_config_creation.sh
-sudo ./service_config_creation.py
 #sudo ./host_config_creation.py
 #sudo ./end_user_management/deploy_end_user_management.sh
 
