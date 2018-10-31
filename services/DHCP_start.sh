4e066ebaca41fa203e448682703f45530cce27fa
plaurens <plaurens@etud.insa-toulouse.fr>
2018-10-31 10:41:15 +0100

    services

--- services/DHCP_start.sh
+++ services/DHCP_start.sh
@@ -2,7 +2,8 @@
 
 # Interfaces
 #DHCP server
-ip address add dev DHCP-eth0 fd00:200:3:1000::547/64
-ip address add dev DHCP-eth0 fd00:300:3:1000::547/64
+#223 hexa = 547 dec qui est le port DHCP
+ip address add dev DHCP-eth0 fd00:200:3:1000::223/64
+ip address add dev DHCP-eth0 fd00:300:3:1000::223/64
 # Gateway
 ip -6 route add ::/0 via fd00:200:3:1000::1 #address of Hall
