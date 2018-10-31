56952208a6e9938b2a02a988c4679c852cf59e6a
Pierrot31 <33099765+Pierrot31@users.noreply.github.com>
2018-10-31 22:12:46 +0100

    der changes

--- service_config_creation.py
+++ service_config_creation.py
@@ -14,23 +14,27 @@ with open(PATH+'service_configuration.json') as data_file:
 
 
 for host, configs in data.items():
-    host_start_config = open("project_cfg/"+host+"_start", "w")
+    host_start_config = open("group3_cfg/"+host+"_start", "w")
     host_start_config.write("#!/bin/bash \n\n")
     host_start_config.write("# This file has been generated automatically, see service_config_creation.py for details. \n\n")
 
     # Interface to LAN
     interface = host+"-eth0"
-    host_start_config.write("ip link set dev "+interface+" up \n")
     for prefix in PREFIXES:
-        host_start_config.write("ip address add dev "+interface+" "+prefix+configs["use_type"]+configs["location"]+configs["forced_address"]+"/64 \n")
+        host_start_config.write("ip address add dev "+interface+" fd00:200:3:"+configs["lan"]+configs["machine_number"]+"/64 \n")
+        host_start_config.write("ip address add dev "+interface+" fd00:300:3:"+configs["lan"]+configs["machine_number"]+"/64 \n")
 
    # Add the default route
-    host_start_config.write("\nip -6 route add ::/0 via "+configs["default_route"]+" \n\n")
+    host_start_config.write("\nip -6 route add ::/0 via "+configs["default_route"]+configs["prefix_default_route"]+" \n\n")
 
     if "bind9" in configs:
         host_start_config.write("named -6 -c /etc/bind/"+configs["bind9"]+".conf \n\n")
+       
+    if "extra_commands" in configs:
+        for command in configs["extra_commands"]:
+            host_start_config.write(command+"\n\n")
 
     host_start_config.close()
     # Add execution right to new file
-    file_stat = os.stat("project_cfg/"+host+"_start")
-    os.chmod("project_cfg/"+host+"_start", file_stat.st_mode | stat.S_IEXEC)
+    file_stat = os.stat("group3_cfg/"+host+"_start")
+    os.chmod("group3_cfg/"+host+"_start", file_stat.st_mode | stat.S_IEXEC)
