6b1fb817d63999b588bd7f3cded4a48c1e087fde
tanguy <ntlecorre@hotmail.fr>
2018-11-01 14:59:35 +0100

    B MAJUSCULE

--- services/deploy_service.sh
+++ services/deploy_service.sh
@@ -3,6 +3,6 @@ PATH_SRC='/home/vagrant/test/group3/lingi2142/services'
 
 
 #sudo chmod 755 /home/vagrant/test/group3/lingi2142/services/DNS/bind/deploy_bind_conf.sh
-sh $PATH_SRC/DNS/bind/deploy_bind_conf.sh
+sh $PATH_SRC/DNS/Bind/deploy_bind_conf.sh
 #chmod 755 ./home/vagrant/test/group3/lingi2142/services/DCHP/deploy_DHCP_conf.sh
 #sh /home/vagrant/test/group3/lingi2142/services/DCHP/deploy_DHCP_conf.sh
