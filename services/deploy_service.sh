afde7bc117f500dbcc695594b9ed23c344b215b5
tanguy <ntlecorre@hotmail.fr>
2018-11-01 13:08:31 +0100

    change path

--- services/deploy_service.sh
+++ services/deploy_service.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-sh /home/vagrant/lingi2142/group3/services/DNS/bind/deploy_bind_conf.sh
-#sh /home/vagrant/lingi2142/group3/services/DCHP/deploy_DHCP_conf.sh
+sh /home/vagrant/test/group3/lingi2142/services/DNS/bind/deploy_bind_conf.sh
+#sh /home/vagrant/test/group3/lingi2142/services/DCHP/deploy_DHCP_conf.sh
