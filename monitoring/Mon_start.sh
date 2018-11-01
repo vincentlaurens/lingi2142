5df007c117c4cee0bde62da36656904016334624
plaurens <plaurens@etud.insa-toulouse.fr>
2018-11-01 17:22:43 +0100

    modify route

--- monitoring/Mon_start.sh
+++ monitoring/Mon_start.sh
@@ -2,11 +2,11 @@
 
 # Interfaces
 #ip link set dev Mon-eth0 up
-ip address add dev Mon-eth0 fd00:200:3:ffff::1/64
-ip address add dev Mon-eth0 fd00:300:3:ffff::1/64
+ip address add dev Mon-eth0 fd00:200:3:1fff::1/64
+ip address add dev Mon-eth0 fd00:300:3:1fff::1/64
 
 # Gateway
-ip -6 route add ::/0 via fd00:200:3:ffff::2 #address of pyth
+ip -6 route add ::/0 via fd00:200:3:1fff::2 #address of pyth
 
 
 # Scripts
