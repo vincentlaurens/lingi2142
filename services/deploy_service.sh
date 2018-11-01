3b7fd15028640bebc78b9d99bbe949be3b8bba52
tanguy <ntlecorre@hotmail.fr>
2018-11-01 14:56:03 +0100

    commit ça

--- services/deploy_service.sh
+++ services/deploy_service.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
+PATH_SRC='/home/vagrant/test/group3/lingi2142/services'
+
 
 #sudo chmod 755 /home/vagrant/test/group3/lingi2142/services/DNS/bind/deploy_bind_conf.sh
-sudo sh DNS/bind/deploy_bind_conf.sh
+sh $PATH_SRC/DNS/bind/deploy_bind_conf.sh
 #chmod 755 ./home/vagrant/test/group3/lingi2142/services/DCHP/deploy_DHCP_conf.sh
 #sh /home/vagrant/test/group3/lingi2142/services/DCHP/deploy_DHCP_conf.sh
