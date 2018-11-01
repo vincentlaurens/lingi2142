cf0401508e15b81b7d00a2701e42fdc9ff563462
tanguy <ntlecorre@hotmail.fr>
2018-11-01 14:30:21 +0100

    sudo

--- services/deploy_service.sh
+++ services/deploy_service.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 sudo chmod 755 /home/vagrant/test/group3/lingi2142/services/DNS/bind/deploy_bind_conf.sh
-sh /home/vagrant/test/group3/lingi2142/services/DNS/bind/deploy_bind_conf.sh
+sudo sh /home/vagrant/test/group3/lingi2142/services/DNS/bind/deploy_bind_conf.sh
 #chmod 755 ./home/vagrant/test/group3/lingi2142/services/DCHP/deploy_DHCP_conf.sh
 #sh /home/vagrant/test/group3/lingi2142/services/DCHP/deploy_DHCP_conf.sh
