04e3ac4e96aa2b3d70a647ead74a0fde04f28f37
plaurens <plaurens@etud.insa-toulouse.fr>
2018-11-01 14:00:44 +0100

    Path correction

--- services/deploy_service.sh
+++ services/deploy_service.sh
@@ -1,2 +1,3 @@
-sh /home/vagrant/lingi2142/services/DNS/bind/deploy_bind_conf.sh
-#sh /home/vagrant/lingi2142/end_user_management/DCHP/deploy_DHCP_conf.sh  
+#!/usr/bin/env bash
+sh /home/vagrant/lingi2142/group3/services/DNS/bind/deploy_bind_conf.sh
+#sh /home/vagrant/lingi2142/group3/services/DCHP/deploy_DHCP_conf.sh
