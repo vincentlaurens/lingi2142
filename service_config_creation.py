03539838f259cecd317ac74564832c1ae0879a2e
Pierrot31 <33099765+Pierrot31@users.noreply.github.com>
2018-11-04 00:09:42 +0100

    need to correct "lan_configs"

--- service_config_creation.py
+++ service_config_creation.py
@@ -19,10 +19,11 @@ for host, configs in data.items():
     # Interface to LAN
     interface = host+"-eth0"
     for prefix_address in PREFIXES_ADDRESS:
-        host_start_config.write("ip address add dev "+interface+" "+prefix_address+"1"+configs["lan"]+configs["machine_number"]+"/64 \n")
+        host_start_config.write("ip address add dev "+interface+" "+prefix_address+configs["City"]+configs["eths"]+lan_configs+"::"+configs["machine_number"]+"
+    
 
    # Add the default route
-    host_start_config.write("\nip -6 route add ::/0 via "+configs["default_route"]+configs["prefix_default_route"]+" \n\n")
+    host_start_config.write("\nip -6 route add ::/0 via "+prefix_address+configs["City"]+configs["eths"]+lan_configs+"::"+configs["prefix_default_route"]+" \n\n")
 
     if "bind9" in configs:
         host_start_config.write("named -6 -c /etc/bind/"+configs["bind9"]+".conf \n\n")
