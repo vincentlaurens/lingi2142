4e2a90708253cd7fb53e1fb1ba39f7a70a7be39c
vlaurens <vlaurens@etud.insa-toulouse.fr>
2018-11-04 15:57:09 +0100

    refactor addressing of routers v2

--- router_config_creation.py
+++ router_config_creation.py
@@ -50,7 +50,7 @@ for router, configs in data.items():
 	if "lans" in configs:
 		for lan, lan_configs in configs["lans"].items():
 			for prefix_address in PREFIXES_ADDRESS:
-				router_start_file.write("ip address add dev "+router+"-"+lan+" "+prefix_address+configs["City"]+configs["router_id"]+lan_configs+"::"+configs["router_id"]+"/64 \n")
+				router_start_file.write("ip address add dev "+router+"-"+lan+" "+prefix_address+configs["City"]+configs["lan_interface"]+lan_configs+"::"+configs["router_id"]+"/64 \n")
 	router_start_file.write("\n")
 
 	##############vlan interfaces #######################################################
