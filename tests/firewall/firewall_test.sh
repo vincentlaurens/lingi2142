11cca915ccc2d8427a966d15a362f6844e695639
plaurens <plaurens@etud.insa-toulouse.fr>
2018-11-30 16:41:09 +0100

    replace b by 3

--- tests/firewall/firewall_test.sh
+++ tests/firewall/firewall_test.sh
@@ -2,14 +2,14 @@
 
 
 
-sudo nmap -6 -sU -sT -p0-1000  fd00:200::b > /dev/null
+sudo nmap -6 -sU -sT -p0-1000  fd00:200::3 > /dev/null
    if [ $? != 0 ]
    then
         echo "port failed"
    else echo "scan terminate succefully"
    fi
 
-sudo nmap -6 -sU -sT -p0-1000  fd00:300::b > /dev/null
+sudo nmap -6 -sU -sT -p0-1000  fd00:300::3 > /dev/null
    if [ $? != 0 ]
    then
         echo "port failed"
@@ -17,84 +17,84 @@ sudo nmap -6 -sU -sT -p0-1000  fd00:300::b > /dev/null
    fi
 
 
-sudo nmap -6 -p 179  fd00:200::b > /dev/null
+sudo nmap -6 -p 179  fd00:200::3 > /dev/null
    if [ $? != 0 ]
    then
         echo "port failed"
    else echo "port 179 open (filtered)"
    fi
 
-sudo nmap -6 -p 179  fd00:300::b > /dev/null
+sudo nmap -6 -p 179  fd00:300::3 > /dev/null
    if [ $? != 0 ]
    then
         echo "port failed"
    else echo "port 179 open (filtered)"
    fi
 
-sudo nmap -6 -p 89  fd00:200::b > /dev/null
+sudo nmap -6 -p 89  fd00:200::3 > /dev/null
    if [ $? != 0 ]
    then
         echo "port failed"
    else echo "port 89 filtered ok"
    fi
 
-sudo nmap -6 -p 89  fd00:300::b > /dev/null
+sudo nmap -6 -p 89  fd00:300::3 > /dev/null
    if [ $? != 0 ]
    then
         echo "port failed"
    else echo "port 89 filtered ok"
    fi
 
-sudo nmap -6 -p 161  fd00:200::b > /dev/null
+sudo nmap -6 -p 161  fd00:200::3 > /dev/null
    if [ $? != 0 ]
    then
         echo "port failed"
    else echo "port 161 filtered ok"
    fi
 
-sudo nmap -6 -p 161  fd00:300::b > /dev/null
+sudo nmap -6 -p 161  fd00:300::3 > /dev/null
    if [ $? != 0 ]
    then
         echo "port failed"
    else echo "port 161 filtered ok"
    fi
 
-sudo nmap -6 -p 162  fd00:200::b > /dev/null
+sudo nmap -6 -p 162  fd00:200::3 > /dev/null
    if [ $? != 0 ]
    then
         echo "port failed"
    else echo "port 162 filtered ok"
    fi
 
-sudo nmap -6 -p 162  fd00:300::b > /dev/null
+sudo nmap -6 -p 162  fd00:300::3 > /dev/null
    if [ $? != 0 ]
    then
         echo "port failed"
    else echo "port 162 filtered ok"
    fi
 
-sudo nmap -6 -p 546  fd00:200::b > /dev/null
+sudo nmap -6 -p 546  fd00:200::3 > /dev/null
    if [ $? != 0 ]
    then
         echo "port failed"
    else echo "port 546 filtered ok"
    fi
 
-sudo nmap -6 -p 546  fd00:300::b > /dev/null
+sudo nmap -6 -p 546  fd00:300::3 > /dev/null
    if [ $? != 0 ]
    then
         echo "port failed"
    else echo "port 546 filtered ok"
    fi
 
-sudo nmap -6 -p 547  fd00:200::b > /dev/null
+sudo nmap -6 -p 547  fd00:200::3 > /dev/null
    if [ $? != 0 ]
    then
         echo "port failed"
    else echo "port 547 filtered ok"
    fi
 
-sudo nmap -6 -p 547  fd00:300::b > /dev/null
+sudo nmap -6 -p 547  fd00:300::3 > /dev/null
    if [ $? != 0 ]
    then
         echo "port failed"
@@ -102,14 +102,14 @@ sudo nmap -6 -p 547  fd00:300::b > /dev/null
    fi
 
 
-sudo nmap -6 -p 23  fd00:200::b > /dev/null
+sudo nmap -6 -p 23  fd00:200::3 > /dev/null
    if [ $? != 0 ]
    then
         echo "port failed"
    else echo "port 547 filtered ok"
    fi
 
-sudo nmap -6 -p 23  fd00:300::b > /dev/null
+sudo nmap -6 -p 23  fd00:300::3 > /dev/null
    if [ $? != 0 ]
    then
         echo "port failed"
