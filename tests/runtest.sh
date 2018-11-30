13ce9bb08fffc80c14cbda23696ea7f6d84fc5b9
plaurens <plaurens@etud.insa-toulouse.fr>
2018-11-30 17:17:36 +0100

    run test

--- tests/runtest.sh
+++ tests/runtest.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+sudo chmod 755 ./routing/BGP_test.sh
+sudo chmod 755 ./routing/OSPF_test.sh
 sudo sh ./routing/BGP_test.sh
-sleep(10)
+sleep 10
 sudo sh ./routing/OSPF_test.sh
-sleep(10)
+sleep 10
