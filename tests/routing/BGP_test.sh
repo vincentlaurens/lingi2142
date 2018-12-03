80519841255d3854ed0ade84601f2ac2c83e12a6
vincentlaurens <18136724+vincentlaurens@users.noreply.github.com>
2018-12-03 20:50:23 +0100

    bgp test

--- tests/routing/BGP_test.sh
+++ tests/routing/BGP_test.sh
@@ -5,7 +5,8 @@
 #Ping if every router ping provider
 
 # 1 ping with 2 sec timeout
-sudo ip netns exec Pyth ping6 -c 2 -W 1 fd00:200::b > /dev/null
+echo "Test can take few minutes"
+sudo ip netns exec Pyth ping6 -c 15 -W 1 fd00:200::b > /dev/null
    if [ $? != 0 ]
    then
         echo "Pyth failed"
