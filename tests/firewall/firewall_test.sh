3114b70fe154cdb114fecd2ff71cd572c2020e68
plaurens <plaurens@etud.insa-toulouse.fr>
2018-12-04 11:45:28 +0100

    test firewall

--- tests/firewall/firewall_test.sh
+++ tests/firewall/firewall_test.sh
@@ -2,121 +2,121 @@
 
 
 
-sudo nmap -6 -sU -sT -p0-1000  fd00:200::3 > /dev/null
+ sudo ip netns exec "Hall" nmap -6 -sU -sT -p0-1000  fd00:200::3 > /dev/null
    if [ $? != 0 ]
    then
         echo "port failed"
    else echo "scan terminate succefully"
    fi
 
-sudo nmap -6 -sU -sT -p0-1000  fd00:300::3 > /dev/null
+ sudo ip netns exec "Hall" nmap -6 -p 179  fd00:200::3 > /dev/null
+   if [ $? != 0 ]
+   then
+        echo "port failed"
+   else echo "port 179 open (filtered)"
+   fi
+
+ sudo ip netns exec "Hall" nmap -6 -p 89  fd00:200::3 > /dev/null
+    if [ $? != 0 ]
+    then
+        echo "port failed"
+    else echo "port 89 filtered ok"
+    fi
+
+ sudo ip netns exec "Hall" nmap -6 -p 161  fd00:200::3 > /dev/null
+   if [ $? != 0 ]
+   then
+        echo "port failed"
+   else echo "port 161 filtered ok"
+   fi
+
+ sudo ip netns exec "Hall" nmap -6 -p 162  fd00:200::3 > /dev/null
+   if [ $? != 0 ]
+   then
+        echo "port failed"
+   else echo "port 162 filtered ok"
+   fi
+
+
+ sudo ip netns exec "Hall" nmap -6 -p 546  fd00:200::3 > /dev/null
+   if [ $? != 0 ]
+   then
+        echo "port failed"
+   else echo "port 546 filtered ok"
+   fi
+
+ sudo ip netns exec "Hall" nmap -6 -p 547  fd00:200::3 > /dev/null
+    if [ $? != 0 ]
+    then
+        echo "port failed"
+    else echo "port 547 filtered ok"
+    fi
+
+ sudo ip netns exec "Hall" nmap -6 -p 23  fd00:200::3 > /dev/null
+   if [ $? != 0 ]
+   then
+        echo "port failed"
+   else echo "port 547 filtered ok"
+   fi
+
+
+ sudo ip netns exec "Pyth" nmap -6 -sU -sT -p0-1000  fd00:300::3 > /dev/null
    if [ $? != 0 ]
    then
         echo "port failed"
    else echo "scan terminate succefully"
    fi
 
-
-sudo nmap -6 -p 179  fd00:200::3 > /dev/null
+ sudo ip netns exec "Pyth" nmap -6 -p 179  fd00:300::3 > /dev/null
    if [ $? != 0 ]
    then
         echo "port failed"
    else echo "port 179 open (filtered)"
    fi
 
-sudo nmap -6 -p 179  fd00:300::3 > /dev/null
-   if [ $? != 0 ]
-   then
-        echo "port failed"
-   else echo "port 179 open (filtered)"
-   fi
-
-sudo nmap -6 -p 89  fd00:200::3 > /dev/null
+ sudo ip netns exec "Pyth"  nmap -6 -p 89  fd00:300::3 > /dev/null
    if [ $? != 0 ]
    then
         echo "port failed"
    else echo "port 89 filtered ok"
    fi
 
-sudo nmap -6 -p 89  fd00:300::3 > /dev/null
-   if [ $? != 0 ]
-   then
-        echo "port failed"
-   else echo "port 89 filtered ok"
-   fi
-
-sudo nmap -6 -p 161  fd00:200::3 > /dev/null
+ sudo ip netns exec "Pyth" nmap -6 -p 161  fd00:300::3 > /dev/null
    if [ $? != 0 ]
    then
         echo "port failed"
    else echo "port 161 filtered ok"
    fi
 
-sudo nmap -6 -p 161  fd00:300::3 > /dev/null
-   if [ $? != 0 ]
-   then
-        echo "port failed"
-   else echo "port 161 filtered ok"
-   fi
-
-sudo nmap -6 -p 162  fd00:200::3 > /dev/null
+ sudo ip netns exec "Pyth"  nmap -6 -p 162  fd00:300::3 > /dev/null
    if [ $? != 0 ]
    then
         echo "port failed"
    else echo "port 162 filtered ok"
    fi
 
-sudo nmap -6 -p 162  fd00:300::3 > /dev/null
-   if [ $? != 0 ]
-   then
-        echo "port failed"
-   else echo "port 162 filtered ok"
-   fi
-
-sudo nmap -6 -p 546  fd00:200::3 > /dev/null
+ sudo ip netns exec "Pyth"  nmap -6 -p 546  fd00:300::3 > /dev/null
    if [ $? != 0 ]
    then
         echo "port failed"
    else echo "port 546 filtered ok"
    fi
 
-sudo nmap -6 -p 546  fd00:300::3 > /dev/null
-   if [ $? != 0 ]
-   then
-        echo "port failed"
-   else echo "port 546 filtered ok"
-   fi
-
-sudo nmap -6 -p 547  fd00:200::3 > /dev/null
+ sudo ip netns exec "Pyth"  nmap -6 -p 547  fd00:300::3 > /dev/null
    if [ $? != 0 ]
    then
         echo "port failed"
    else echo "port 547 filtered ok"
    fi
 
-sudo nmap -6 -p 547  fd00:300::3 > /dev/null
+ sudo ip netns exec "Pyth"  nmap -6 -p 23  fd00:300::3 > /dev/null
    if [ $? != 0 ]
    then
         echo "port failed"
    else echo "port 547 filtered ok"
    fi
 
-
-sudo nmap -6 -p 23  fd00:200::3 > /dev/null
-   if [ $? != 0 ]
-   then
-        echo "port failed"
-   else echo "port 547 filtered ok"
-   fi
-
-sudo nmap -6 -p 23  fd00:300::3 > /dev/null
-   if [ $? != 0 ]
-   then
-        echo "port failed"
-   else echo "port 547 filtered ok"
-   fi
-
-sudo nmap -6 -p 23  fd00:300:3:f00::1 > /dev/null
+ sudo ip netns exec "Pyth"  nmap -6 -p 23  fd00:300:3:f00::1 > /dev/null
    if [ $? != 0 ]
    then
         echo "port failed"
