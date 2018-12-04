abc4390ed5823be8742797dd22e6263e92b948c6
vincentlaurens <18136724+vincentlaurens@users.noreply.github.com>
2018-12-04 13:29:08 +0100

    tunnel tests

--- tests/routing/TUNNELING_test.sh
+++ tests/routing/TUNNELING_test.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
 echo "[Test TUNNEL]"
+
+echo "[Test TUNNEL] Test can take few minutes"
+echo "[Test TUNNEL] On Hall see trace on /tmp/tunnel.pcap"
+echo "[Test TUNNEL] Open it with wireshark"
+sudo ip netns exec Hall tcpdump -ni any -w /tmp/tunnel.pcap
+
+echo "[Test TUNNEL] ping6 from Pyth to Hall provider"
+sudo ip netns exec Pyth ping6 -c 15 -W 1 fd00:200::b > /dev/null
