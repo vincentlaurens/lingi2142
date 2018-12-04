6a96bd066408462d4fff38ee3d98746ec64b406d
vincentlaurens <vincent.laurens@student.uclouvain.be>
2018-12-04 19:50:09 +0100

    test tunnel not tested

--- tests/routing/TUNNELING_test.sh
+++ tests/routing/TUNNELING_test.sh
@@ -3,8 +3,8 @@
 echo "[Test TUNNEL]"
 
 echo "[Test TUNNEL] Test can take few minutes"
-echo "[Test TUNNEL] On Hall see trace on /tmp/tunnel.pcap"
-echo "[Test TUNNEL] Open it with wireshark"
+echo "[Test TUNNEL] On Hall see trace on /tmp/tunneltoHall.pcap"
+echo "[Test TUNNEL] Open it with command: sudo tcpdump -ttttnnr /tmp/tunneltoHall.pcap"
 sudo ip netns exec Hall tcpdump -ni any -w /tmp/tunneltoHall.pcap
 
 echo "[Test TUNNEL] ping6 from Pyth to Hall provider"
@@ -12,8 +12,8 @@ sudo ip netns exec Pyth ping6 -c 15 -W 1 fd00:200::b > /dev/null
 
 
 echo "[Test TUNNEL] Test can take few minutes"
-echo "[Test TUNNEL] On Hall see trace on /tmp/tunnel.pcap"
-echo "[Test TUNNEL] Open it with wireshark"
+echo "[Test TUNNEL] On Hall see trace on /tmp/tunneltoPyth.pcap"
+echo "[Test TUNNEL] Open it with command: /tmp/tunneltoPyth.pcap"
 sudo ip netns exec Pyth tcpdump -ni any -w /tmp/tunneltoPyth.pcap
 
 echo "[Test TUNNEL] ping6 from Pyth to Hall provider"
