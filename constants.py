11cf643247ce73d3cab65c320b8667d64ab3343a
vincentlaurens <18136724+vincentlaurens@users.noreply.github.com>
2018-12-06 16:17:09 +0100

    final on vagrant 2

--- constants.py
+++ constants.py
@@ -1,5 +1,5 @@
 #Python 3 : Define the constants
 
-PATH = "//home/vagrant/lingi2142"
+PATH = "/home/vagrant/lingi2142"
 PREFIXES_ADDRESS = ["fd00:200:3:", "fd00:300:3:"]
 VLAN_USES = ["2", "3", "5", "6"]
