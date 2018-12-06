cabfe30883a254a588a434b25fa3f6646d5671a3
vincentlaurens <vincent.laurens@student.uclouvain.be>
2018-12-06 10:26:22 +0100

    routing tests tunnel add ping to have a bigger trace

--- tests/routing/TUNNELING_test.sh
+++ tests/routing/TUNNELING_test.sh
@@ -4,23 +4,23 @@ echo "[Test TUNNEL]"
 
 echo "[Test TUNNEL] Test can take few minutes"
 echo "[Test TUNNEL] On Hall see trace on /tmp/tunneltoHall.pcap"
-echo "[Test TUNNEL] Open it with command: sudo tcpdump -ttttnnr /tmp/tunneltoHall.pcap"
-sudo ip netns exec Hall sudo tcpdump -ni any -w /tmp/tunneltoHall.pcap -ttt &
+echo "[Test TUNNEL] Open it with command: sudo tcpdump -vvttttnnr /tmp/tunneltoHall.pcap"
+sudo ip netns exec Pyth tcpdump -ni any -w /tmp/tunneltoHall.pcap -ttttnnvvS -n dst fd00:200::b &
 sleep 1
 echo "[Test TUNNEL] ping6 from Pyth to Hall provider"
-sudo ip netns exec Pyth ping6 -c 15 -W 1 fd00:200::b > /dev/null
+sudo ip netns exec Pyth ping6 -c 200 -W 1 fd00:200::b > /dev/null
 
 
 echo "[Test TUNNEL] Test can take few minutes"
 echo "[Test TUNNEL] On Hall see trace on /tmp/tunneltoPyth.pcap"
-echo "[Test TUNNEL] Open it with command: /tmp/tunneltoPyth.pcap"
-sudo ip netns exec Pyth sudo tcpdump -ni any -w /tmp/tunneltoPyth.pcap -ttt &
-seep1
+echo "[Test TUNNEL] Open it with command: sudo tcpdump -ttttnnr /tmp/tunneltoPyth.pcap"
+sudo ip netns exec Hall tcpdump -ni any -w /tmp/tunneltoPyth.pcap -ttttnnvvS -n dst fd00:200::b &
+sleep 1
 
 echo "[Test TUNNEL] ping6 from Pyth to Hall provider"
-sudo ip netns exec Hall ping6 -c 15 -W 1 fd00:300::b > /dev/null
+sudo ip netns exec Hall ping6 -c 200 -W 1 fd00:300::b > /dev/null
 
 sleep 1
-pkill tcpdump
+sudo pkill tcpdump
 
 echo "[Test TUNNEL] End of script"
